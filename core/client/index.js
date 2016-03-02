8238c1e727ffc117dc8e62ebedf706f7e4717719
andy-williams <williams.andrew.lirio@gmail.com>
2016-03-02 22:28:22 +0000

    fix bootstrap

--- core/client/index.js
+++ core/client/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import { updatePostList } from './actions/app';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { updatePostList } from './actions/app';
 
+// containers
+import App from './views/containers/app';
 import PostListPreview from './views/containers/post-list-preview';
 import { PostEditRouteHandler } from './views/containers/post-edit';
 
 const history = syncHistoryWithStore(browserHistory, store);
-
-import { Component } from 'react';
 
 render(
   <Provider store={store}>
@@ -25,4 +25,5 @@ render(
   document.getElementById('app-container')
 );
 
+// init dispatch
 store.dispatch(updatePostList());
